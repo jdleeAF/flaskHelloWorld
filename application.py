8494f1d4e492acddf81794a2440602b3aee951d0
Jeff Lee <lee20@g.harvard.edu>
2016-11-14 00:06:25 -0500

    Adding a new route for name

--- application.py
+++ application.py
@@ -6,5 +6,9 @@ application = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@application.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
+
 if __name__ == '__main__':
     application.run()
